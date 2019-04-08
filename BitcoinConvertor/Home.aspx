5e5568288cbf01a8f8a4547c6c575d8cf19294ec
hirensk93 <46274035+hirensk93@users.noreply.github.com>
2019-04-08 10:33:36 +0530

    Bitcoin Currency to Country Currency

---
+++ BitcoinConvertor/Home.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Bitcoin Convertor</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Currency:"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddl_Currency" runat="server" AutoPostBack="True">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Value:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txt_val" runat="server"></asp:TextBox>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btn_Convert" runat="server" OnClick="btn_Convert_Click" Text="Convert" />
+        <br />
+        <asp:Label ID="lbl_res" runat="server" Text=""></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ BitcoinConvertor/Home.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+public partial class Home : System.Web.UI.Page
+{
+    String str;
+    WebClient wc;
+    SqlConnection sc;
+    SqlCommand scmd;
+    SqlDataAdapter sda;
+    String cstr;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            cstr = ConfigurationManager.ConnectionStrings["cstr"].ConnectionString;
+            sc = new SqlConnection(cstr);
+            scmd = new SqlCommand("select * from Currency", sc);
+            sda = new SqlDataAdapter(scmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            ddl_Currency.DataSource = dt;
+            ddl_Currency.DataBind();
+            ddl_Currency.DataTextField = "Country_Name";
+            ddl_Currency.DataValueField = "Country_Code";
+            ddl_Currency.DataBind();
+        }
+    }
+
+    protected void btn_Convert_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            String url;
+            url = "https://blockchain.info/tobtc?" + "currency=" + Server.UrlEncode(this.ddl_Currency.SelectedItem.Value.ToString()) + "&value=" + Server.UrlEncode(this.txt_val.Text.ToString());
+            wc = new WebClient();
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            //str = wc.DownloadString("https://blockchain.info/ticker");
+            str = wc.DownloadString(url);
+            lbl_res.Text = str.ToString();
+        }
+        catch(Exception ex)
+        {
+            lbl_res.Text = "Your Country isn't available" + ex.Message.ToString();
+        }
+
+       
+       
+    }
+}
